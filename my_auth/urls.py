f993dab637feefb52aaf0a051606089f412bea84
Rusich <vasenin@verqor.com>
2025-07-20 21:25:34 +0300

    added forms

--- my_auth/urls.py
+++ my_auth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import LoginView, RegView
+from .views import LoginView, RegView, logout_view
 
 app_name = 'my_auth'
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('registration/', RegView.as_view(), name='registration'),
+    path('logout/', logout_view, name='logout'),
 ]
--- my_auth/views.py
+++ my_auth/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 class LoginView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
@@ -9,4 +11,9 @@ class LoginView(View):
 class RegView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, 'my_auth/registration.html')
+
+@login_required
+def logout_view(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect('/auth/login')
 
